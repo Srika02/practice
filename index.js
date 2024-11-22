cc1c667d2faa6b445689c3b1beb87bfeac9ebf08
Srika02 <srikanthbhattachariya@outlook.com>
2024-11-22 13:14:39 +0530

    .
    .

---
+++ index.js
@@ -0,0 +1,38 @@
+// const srikanth_raghavan="2";
+
+// console.log(srikanth_raghavan);
+// let a=1;
+// console.log(a);
+// console.log('hi how are you');
+// console.log('hi how are you');
+// var c=4;
+// //let ar=[12,14,16,18]
+// console.log(ar[2])
+// let ao=["hi","hello","hey"]
+// console.log(ao[1]);
+// let ob={name:1};
+// console.log(ob.name);
+// var afh=[{abc:123},"hi",2]
+// console.log(afh[0].abc);
+// console.log(`hisri ${a}`)
+
+// for(let i of ar){
+//     console.log(i)
+// }
+let a =[
+    1,2,3,41,51,61,71]
+// for (i=4; i<list.length; i++){
+//     console.log(list[i])
+// }
+for (let i of a ) {
+    console.log(i)
+}
+if(0<a.length){
+    console.log("gmgkug")
+}
+if (5<a.length){
+    console.log("hiiieee")
+}
+else {
+    console.log("jie")
+}
